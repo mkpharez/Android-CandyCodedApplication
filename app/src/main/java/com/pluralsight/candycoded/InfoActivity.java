32c1424822fb3322fbfdd5acb3786a41ce374ef4
mkpharez <65944009+mkpharez@users.noreply.github.com>
2020-09-27 12:53:33 +0300

    Code to start ThePhoneActivity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -39,5 +39,10 @@ public class InfoActivity extends AppCompatActivity {
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
+    public void createPhoneIntent(View view){
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(callIntent);
+    }
     // ***
 }
